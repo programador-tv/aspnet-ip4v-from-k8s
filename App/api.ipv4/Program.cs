5141ecc95a2ac9597a7b017239dc77260f0ef985
eduardoworrel <eduardo.worrel@gmail.com>
2023-06-22 23:57:56 -0300

    feat: :art: minimal api getting ipv4

---
+++ App/api.ipv4/Program.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+EnableForwardedHeaders(app);
+app.MapGet("/", (ctx) => {
+
+    var ipv4 = string.Empty;
+    var remoteIpAddress = ctx.Connection.RemoteIpAddress;
+    if(remoteIpAddress != null)
+    {
+        ipv4 = remoteIpAddress.ToString();
+    }
+    _ = ctx.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(ipv4));
+    return Task.CompletedTask;
+});
+app.MapGet("/hello", () => {
+
+    return "Hello World!";
+
+});
+
+
+
+app.Run();
+
+
+static void EnableForwardedHeaders(IApplicationBuilder app)
+{
+    
+    var forwardedHeadersOptions = new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+        RequireHeaderSymmetry = false
+    };
+    forwardedHeadersOptions.KnownNetworks.Clear();
+    forwardedHeadersOptions.KnownProxies.Clear();
+    app.UseForwardedHeaders(forwardedHeadersOptions);
+
+}
